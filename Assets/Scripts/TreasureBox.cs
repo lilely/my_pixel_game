7f8517b394a3b43ba02bd32ae577ade38c1269ac
lilely <lilely@163.com>
2023-01-28 17:14:53 +0800

    增加宝箱

---
+++ Assets/Scripts/TreasureBox.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureBox : MonoBehaviour
+{
+    public GameObject Item;
+    public float gainDelayTime;
+
+    private Animator anim;
+    private bool canOpenBox;
+    private bool opened;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canOpenBox = true;
+        opened = false;
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canOpenBox && !opened) {
+            if(Input.GetKeyDown(KeyCode.I)) {
+                opened = true;
+                anim.SetTrigger("opening");
+                Invoke("GainItem", gainDelayTime);
+            }
+        }
+    }
+
+    void GainItem() {
+        Instantiate(Item, transform.position, Quaternion.identity);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Player")) {
+            canOpenBox = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.gameObject.CompareTag("Player")) {
+            canOpenBox = false;
+        }
+    }
+}
