e166d7c5be92387dec13be7e1216f78184b6e2fe
lilely <lilely@163.com>
2023-01-31 18:12:07 +0800

    Canvas里面增加loading

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,28 @@ using System.Net.Mime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    public void StartAction() {
-        SceneManager.LoadScene(1);
+    public Slider slider;
+    public Text progressText;
+    public GameObject loadingScreen;
+
+    public void LoadLevel(int sceneIndex) {
+        StartCoroutine(AsyncLoadLevel(sceneIndex));
+    }
+
+    IEnumerator AsyncLoadLevel(int sceneIndex) {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+        while(! operation.isDone) {
+            float progress = operation.progress / 0.9f;
+            slider.value = progress;
+            progressText.text = Mathf.FloorToInt(progress * 100f).ToString() + "%";
+            yield return null;
+        }
     }
 
     public void QuitAction() {
