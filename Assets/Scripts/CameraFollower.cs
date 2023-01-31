ac00d7ac3a5ff03514098d23e9c0b03b010ad904
lilely <lilely@163.com>
2023-02-01 00:27:32 +0800

    解决Camera follow的bug

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -21,12 +21,12 @@ public class CameraFollower : MonoBehaviour
     void LateUpdate() {
         if(target != null) {
             if(transform.position != target.position) {
-                Vector3 targetPos = target.position;
-                targetPos.x += offsetX;
-                targetPos.y += offsetY;
+                Vector2 targetPos = target.position;
                 targetPos.x = Mathf.Clamp(targetPos.x, minPosition.x, maxPosition.x);
                 targetPos.y = Mathf.Clamp(targetPos.y, minPosition.y, maxPosition.y);
-                transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
+                transform.position = Vector2.Lerp(transform.position, targetPos, smoothing);
+                Debug.Log("camera pos: " + transform.position.x.ToString() + " : " + transform.position.y.ToString());
+                Debug.Log("player pos: " + target.position.x.ToString() + " : " + target.position.y.ToString());
             }
         }
     }
